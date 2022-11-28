69db50a249004895ff69b4dcbe1ce886068dda5f
ha77y <114589921+ha77y@users.noreply.github.com>
2022-11-28 22:24:57 +0000

    Update blungo_walk.cs

--- Assets/scripts/blungo_walk.cs
+++ Assets/scripts/blungo_walk.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class blungo_walk : MonoBehaviour
 {
+    public float movementX;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,9 +15,24 @@ public class blungo_walk : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.position = new Vector3(transform.position.x + 5* Input.GetAxis("Horizontal") * Time.deltaTime, transform.position.y, transform.position.z);
-        // transform.position = this objects position 
-        // Vector3 = how coordinates are stored aka Vector3(x,y,z)
-        //Input.GetAxis("Horizontal") gives us a float value for the horizontal axis -1 to 1 , left to right 
+        if (Input.GetKey("d"))
+        {
+            movementX = 1f;
+        }
+        else if (Input.GetKey("a"))
+        {
+            movementX = -1f;
+        }
+        else
+        {
+            movementX = 0f;
+        }
+            transform.position = new Vector3(transform.position.x + 5f * movementX * Time.deltaTime, transform.position.y, transform.position.z);
+        }
+       
+        //transform.position = this objects position 
+        //Vector3 = how coordinates are stored aka Vector3(x,y,z)
+        //Input.GetAxis("Horizontal") gives us a float value for the horizontal axis -1f to 1f , left to right
+        //Time.deltaTime is the time between updates aka the time between frames 
     }
-}
+
